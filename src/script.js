72e7dbf7ac77685c02f646d467be26b2b187b5a5
Banie456 <sinqobile.myeki@gmail.com>
2024-10-19 01:03:03 +0200

    added the weather forecast from java script instead of HTML

--- src/script.js
+++ src/script.js
@@ -52,7 +52,31 @@ function updateCity(event) {
   searchCity(cityInput);
 }
 
+function displayWeatherForecast() {
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thu"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `<div class="weather-forecast-day">
+  <div class="weather-forecast-date">${day}</div>
+  <div class="weather-forecast-icon">🌤️</div>
+  <div class="weather-forecast-temperatures">
+    <div class="weather-forecast-temperature">
+      <strong>15º</strong>
+    </div>
+    <div class="weather-forecast-temperature">9º</div>
+  </div>
+</div>`;
+  });
+
+  let weatherForecastElement = document.querySelector("#weather-forecast");
+  weatherForecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", updateCity);
 
 searchCity("Durban");
+displayWeatherForecast();
